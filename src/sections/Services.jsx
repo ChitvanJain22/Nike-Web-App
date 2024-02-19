4059f418b10a1c5f91ccc3baac9744dae4b9a581
ChitvanJain22 <torrid22.cj@gmail.com>
2024-02-19 23:54:11 +0530

    save it

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,12 @@
+import { services } from "../constants";
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((services) => (
+        <serviceCard key={services.label} {...services} />
+      ))}
+    </section>
+  );
+};
 
-export default Services
+export default Services;
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,3 +1,4 @@
+import { shoe8 } from "../assets/images";
 import Button from "../components/Button";
 
 const SuperQuality = () => {
@@ -9,19 +10,33 @@ const SuperQuality = () => {
       <div className="flex flex-col flex-1">
         <h2
           className="font-palanquin text-4xl capitalize
-        font-bold"
+        font-bold lg:max-w-lg"
         >
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
-            The New Arrival
-          </span>
+          We Provide You
+          <span className="text-coral-red"> Super</span>
           <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+          <span className="text-coral-red">Quality </span>Shoes
         </h2>
-        <p className=" font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
-          Discover Stylish Nike arrivals, quality confort, and innovation for
-          yuor active life.
+        <p className=" mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
         </p>
-        <Button label="View Details" />
+        <p className=" mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11">
+          <Button label="View Details" />
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={shoe8}
+          alt="shoe8"
+          width={570}
+          height={522}
+          className=" object-contain"
+        />
       </div>
     </section>
   );
