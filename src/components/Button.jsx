7c91468edd46ca13a3a5b8bc4487fee18e292d93
ChitvanJain22 <torrid22.cj@gmail.com>
2024-02-22 00:01:36 +0530

    Completed :>

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,6 +4,7 @@ const Button = ({
   backgroundColor,
   textColor,
   borderColor,
+  fullWidth,
 }) => {
   return (
     <button
@@ -13,7 +14,7 @@ const Button = ({
         ? `${backgroundColor} ${textColor} ${borderColor}`
         : "bg-coral-red text-white border-coral-red"
     }
-    rounded-full w-full"}`}
+    rounded-full ${fullWidth && "w-full"}"}`}
     >
       {label}
       {iconURL && (
--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,6 +1,6 @@
 import { star } from "../assets/icons";
 
-const ReviewCard = (imgURL, customerName, rating, feedback) => {
+const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
   return (
     <div className="flex justify-center items-center flex-col">
       <img
@@ -10,7 +10,13 @@ const ReviewCard = (imgURL, customerName, rating, feedback) => {
       />
       <p className="mt-6 max-w-sm text-center info-text">{feedback}</p>
       <div className="mt-3 flex justify-center items-center gap-2.5">
-        <img src={star} width={24} height={24} className="object-contain m-0" />
+        <img
+          src={star}
+          width={24}
+          height={24}
+          alt="rating star"
+          className="object-contain m-0"
+        />
         <p className="text-xl font-montserrat text-slate-gray">({rating})</p>
       </div>
       <h3 className="mt-1 font-palanquin text-3xl text-cneter font-bold">
