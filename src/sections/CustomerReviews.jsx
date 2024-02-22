02148ac1363ed0f6c7dc009b6e796ff1443a6525
ChitvanJain22 <torrid22.cj@gmail.com>
2024-02-22 16:29:12 +0530

    completed finally

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -6,7 +6,7 @@ const CustomerReviews = () => {
     <section className="max-container">
       <h3 className="font-palanquin text-center text-4xl font-bold">
         What Our?
-        <span className="text-coral-red">Customers</span>
+        <span className="text-coral-red"> Customers </span>
         Say?
       </h3>
       <p className="mt-4 mx-w-lg text-center info-text m-auto">
